6f9bdc133adb6a5e4f0470636e4daabff169b9c0
YunYouJun <me@yunyoujun.cn>
2022-12-08 04:01:30 +0800

    chore: add netdun cdn support

---
+++ components/YunFooter.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue'
+
+const netdun = {
+  name: '网盾星球',
+  url: 'https://www.netdun.net',
+}
+</script>
+
+<template>
+  <YunFooter>
+    由 <a :href="netdun.url" target="_blank" :title="netdun.name">{{ netdun.name }}</a>  提供 CDN 支持
+  </YunFooter>
+</template>
