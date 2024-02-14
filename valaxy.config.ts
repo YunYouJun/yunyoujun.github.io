9dfd27903ab76d5f54830c0bb74b5e1cc9912f9b
YunYouJun <me@yunyoujun.cn>
2024-02-15 01:00:51 +0800

    chore: upgrade deps

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -18,45 +18,6 @@ export default defineValaxyConfig<UserThemeConfig> ({
     },
   },
 
-  themeConfig: {
-    banner: {
-      enable: true,
-      title: '云游君的小站',
-      cloud: {
-        enable: true,
-      },
-    },
-
-    pages: [
-      {
-        name: '我的小伙伴们',
-        url: '/links/',
-        icon: 'i-ri-genderless-line',
-        color: 'dodgerblue',
-      },
-      {
-        name: '赞助者们',
-        url: 'https://sponsors.yunyoujun.cn',
-        icon: 'i-ri-heart-line',
-        color: 'red',
-      },
-      {
-        name: '喜欢的女孩子',
-        url: '/girls/',
-        icon: 'i-ri-women-line',
-        color: 'hotpink',
-      },
-    ],
-
-    footer: {
-      since: 2016,
-      beian: {
-        enable: true,
-        icp: '苏ICP备17038157号',
-      },
-    },
-  },
-
   unocss: {
     safelist: [
       'i-ri-home-line',
